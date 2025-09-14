9c75afe76e679e81b58cad181f2a6a78fe91930d
Nelson Rotimi <nelsonabieno@gmail.com>
2025-09-14 18:52:59 +0100

    added style changes for flexbox

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Button, Alert, View, Dimensions, useWindowDimensions } from "react-native";
+import { View } from "react-native";
 
-export default function App() {
-  console.log("====")
-  console.log(useWindowDimensions());
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ 
-        backgroundColor: "dodgerblue",
-        width: "100%",
-        height: "30%"
-        }}
-        ></View>
-      <Button color="orange" title="Click Me" onPress={()=>Alert.alert("My title", "My Message", [{text: "Yes", onPress: ()=> {console.log("Yes")}}, {text: "No", onPress: ()=> {console.log("No")}}])}/>
-      <Button 
-        title="Click Me for a Prompt"
-        onPress={() => Alert.prompt("My title", "My message", text => console.log(text))}
-      />
-    </SafeAreaView>
-  );
+export default function App(){
+  return(
+    <View
+      style={{
+        backgroundColor: "#fff", 
+        flex: 1,
+        flexDirection: "row", // horizontal
+        justifyContent: "center", //main
+        alignItems: "center", //secondary
+        flexWrap: "wrap"
+      }}>
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          }}/>
+        <View style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100
+          }}/>
+        <View style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100
+          }}/>
+        <View style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100
+          }}/>
+        <View style={{
+          backgroundColor: "greenyellow",
+          width: 100,
+          height: 100
+          }}/>
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
