c49d6116aea6af146f53e225083eb762f771d690
Nelson Rotimi <nelsonabieno@gmail.com>
2025-09-23 20:30:31 +0100

    add welcome screen, view image screen and refactor color codes

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ImageBackground, StyleSheet } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+// const bgImage = {require("./assets/background.jpg")}
 
 export default function App(){
   return(
-    <View
-      style={{
-        backgroundColor: "#fff", 
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", //main
-        alignItems: "center", //secondary
-        flexWrap: "wrap"
-      }}>
-        <View style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          }}/>
-        <View style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100
-          }}/>
-        <View style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100
-          }}/>
-        <View style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100
-          }}/>
-        <View style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100
-          }}/>
-    </View>
-  )
-}
+   <ViewImageScreen/>
+  )}
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View,ImageBackground,StyleSheet,Image, Text, ViewComponent } from "react-native";
+
+function WelcomeScreen(props){
+    return(
+     <ImageBackground 
+        source={require("../assets/background.jpg")} 
+        resizeMode="cover" 
+        style={styles.background}>
+        <View style={styles.logoContainer}>
+            <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
+            <Text style={styles.logoText}>Sell What You Don't Need</Text>
+        </View>
+        <View style={styles.loginButton}></View>
+        <View style={styles.registerButton}></View>
+     </ImageBackground>
+    );
+};
+
+const styles =StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    loginButton:{
+        width: "100%",
+        height: 70,
+        backgroundColor: "#fc5c65"
+    },
+    registerButton:{
+        width: "100%",
+        height: 70,
+        backgroundColor: "#4ecdc4"
+    },
+    logo:{
+        width: 100,
+        height: 100
+    },
+    logoContainer:{
+        position: "absolute",
+        top: 70,
+    },
+    logoText: {
+        position: "absolute",
+        top: 90
+    }
+})
+
+export default WelcomeScreen;
+
