deea704d43e90113a6ed14c5a9afb7b24d7541cb
Nelson Rotimi <nelsonabieno@gmail.com>
2025-10-01 17:26:52 +0100

    add custom encapsulating styles

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import AppText from './app/components/AppText';
+
 function App(props) {
   return (
     <View
@@ -9,20 +11,7 @@ function App(props) {
         justifyContent: "center",
         alignItems: "center",
       }}>
-      <Text
-        style={{
-          fontFamily: "Roboto",
-          fontSize: 30,
-          fontStyle: "italic",
-          fontWeight: "600",
-          color: "tomato",
-          textTransform: "capitalize",
-          textAlign: "center",
-          lineHeight: "30"
-        }}
-      >
-        I love React Native ❤️! This is my first React Native App. Here is so more text
-      </Text>
+        <AppText> I love React Native!</AppText>
     </View>
   );
 }
