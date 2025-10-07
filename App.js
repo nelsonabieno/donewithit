9b11193feaccbda0e8452f786d2a10f1df16fc6d
Nelson Rotimi <nelsonabieno@gmail.com>
2025-10-07 12:51:11 +0100

    build card component and add asset images

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { View } from 'react-native';
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import AppButton from './app/components/AppButton';
-
-
+import Card from './app/components/Card';
 
 function App(props) {
   return (
-    <WelcomeScreen/>
+   <View style={{
+    backgroundColor: '#f8f4f4',
+    padding: 20,
+    paddingTop: 100
+   }}>
+    <Card
+      title="Red jacket for sale"
+      subTitle="$100"
+      image={require('./app/assets/jacket.jpg')}
+    />
+   </View>
   );
 }
 
