0a8a3e2479b80b0fef5f219ce49443c0c986a5f2
Nelson Rotimi <nelsonabieno@gmail.com>
2025-12-02 14:04:01 +0100

    extend listitem component

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/screens/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 function App(props) {
   return (
     <Screen>
-      <Icon name="email" size={100} backgroundColor="red" iconColor="white" />
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
